31f506afc820e290322cb1a7b309d31e4a44ba5d
alexsavinov <spell477@gmail.com>
2024-03-13 14:07:59 +0200

    1st commit

--- src/main/java/api/config/BaseTest.java
+++ src/main/java/api/config/BaseTest.java
@@ -1,13 +1,6 @@
 package api.config;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import utils.PropertiesReader;
 
 public class BaseTest {
